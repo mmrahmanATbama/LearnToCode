3920367ef8088bace07b65e36394e9a675c13e51
admin <admin@localhost.localdomain>
2020-02-07 20:02:30 -0700

    C: initial commit for pointerDeclaration.c

---
+++ pointerDeclaration.c
@@ -0,0 +1,15 @@
+/*
+ * Pointer in c. This program shows how to declare, assign and dereference pointers
+ */
+ 
+
+#include<stdio.h>
+
+
+int main()
+{
+  // pointer and data type
+
+
+return 0;
+}
